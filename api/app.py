7228e9b29a43ef49c17d76fd525c9934e1fc4b0a
Mujeeb Khan <mujeebblouch7036@gmail.com>
2024-09-06 12:00:36 +0500

    FAST API

---
+++ api/app.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from langchain.prompts import ChatPromptTemplate
+from langserve import add_routes
+import uvicorn
+from langchain_community.llms import Ollama
+
+
+app=FastAPI(
+    title="Langchain Server",
+    version="1.0",
+    discription="A simple API Server"
+
+)
+
+
+##ollama llama2
+llm=Ollama(model="llama2")
+
+prompt1=ChatPromptTemplate.from_template("Write me an essay about {topic} with 100 words")
+prompt2=ChatPromptTemplate.from_template("Write me an poem about {topic} for a 5 years child with 100 words")
+
+add_routes(
+    app,
+    prompt1|llm,
+    path="/essay"
+)
+
+add_routes(
+    app,
+    prompt2|llm,
+    path="/poem"
+)
+
+
+if __name__=="__main__":
+    uvicorn.run(app,host="localhost",port=8000)
